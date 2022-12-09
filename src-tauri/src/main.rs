de570add3342d8b283bec060f9d15483ac7cf130
Eray Alkış <lolpo12345@outlook.com>
2022-12-09 23:36:59 +0300

    Create base

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,8 @@
 
 use std::collections::HashMap;
 
+use walkdir::WalkDir;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -12,13 +14,53 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn return_map() -> HashMap<String, String> {
+fn create_db_if_not_exists() {
+    let conn = sqlite::open("entries.db").expect("Error while accessing database");
+    let query = "
+        CREATE TABLE IF NOT EXISTS FILES (file_name TEXT, file_type TEXT, path TEXT, parent_path TEXT, is_dir INTEGER, byte_size INTEGER)
+    ";
+    conn.execute(query).expect("Error while executing SQL statement");
+}
+
+#[tauri::command]
+fn return_map() -> HashMap<String, i32> {
+    let conn = sqlite::open("entries.db");
+    let mut test_hashmap = HashMap::new();
+    test_hashmap.insert(String::from("Blue"), 0);
+    test_hashmap.insert(String::from("Green"), 1);
+    test_hashmap.insert(String::from("Red"), 2);
+
+    return test_hashmap;
+}
+
+#[tauri::command]
+fn walk_and_save(base_dir: &str) {
+    let conn = sqlite::open("entries.db").expect("Error while accessing database");
+    for entry in WalkDir::new(base_dir) {
+        let entry = entry.unwrap();
+        let entry_path = entry.into_path();
+        let entry_metadata = entry.metadata().expect("Error while reading entry metadata");
     
+        let base_statement = "INSERT INTO FILES (file_name, file_type, path, parent_path, is_dir, byte_size) VALUES (?, ?, ?, ?, ?, ?)";
+        let query = conn.prepare(base_statement).expect("Error while preparing SQL statement");
+
+        let path = entry_path.extension().expect("Error while reading path extension").to_str();
+        let parent_path = entry_path.parent().expect("Error while raeding parent path").to_str();
+        let is_dir = entry_metadata.is_dir() as i64;
+
+        query.bind((1, entry.file_name().to_str())).unwrap();
+        query.bind((2, path)).unwrap();
+        query.bind((3, entry.path().to_str())).unwrap();
+        query.bind((4, parent_path)).unwrap();
+        query.bind((5, is_dir)).unwrap();
+        query.bind((6, entry_metadata.len() as i64)).unwrap();
+
+    }
 }
 
 fn main() {
     let app = tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, return_map, walk_and_save])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
