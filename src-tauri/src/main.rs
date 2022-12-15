2a10015512a914cff6154260fb37239086a958d6
Eray Alkış <lolpo12345@outlook.com>
2022-12-15 04:11:39 +0300

    Add foreign key constraints to tables

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -52,10 +52,12 @@ fn create_db_if_not_exists(to: &str) {
     let conn = sqlite::open(to).expect("Error while accessing database");
     let query = "
         PRAGMA journal_mode=WAL;
+        PRAGMA foreign_keys=ON;
         CREATE TABLE IF NOT EXISTS FILES (ID INTEGER PRIMARY KEY AUTOINCREMENT, file_name TEXT, file_type TEXT, path TEXT, parent_path TEXT, is_dir INTEGER, is_base_dir INTEGER, byte_size INTEGER);
-        CREATE TABLE IF NOT EXISTS TAGS (ID INTEGER PRIMARY KEY AUTOINCREMENT, tag_name TEXT, parent_path TEXT, parent_id INTEGER, color TEXT);
-        CREATE TABLE IF NOT EXISTS DEADLINES (ID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, date DATETIME, parent_path TEXT, parent_id INTEGER);
+        CREATE TABLE IF NOT EXISTS TAGS (ID INTEGER PRIMARY KEY AUTOINCREMENT, tag_name TEXT, parent_path TEXT, parent_id INTEGER, color TEXT, FOREIGN KEY(parent_id) REFERENCES TAGS(ID));
+        CREATE TABLE IF NOT EXISTS DEADLINES (ID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, date DATETIME, parent_path TEXT, parent_id INTEGER, FOREIGN KEY(parent_id) REFERENCES TAGS(ID));
         CREATE UNIQUE INDEX IF NOT EXISTS unique_file_index ON FILES (file_type, path);
+        CREATE UNIQUE INDEX IF NOT EXISTS unique_tag_index ON TAGS (parent_id, tag_name);
         CREATE INDEX IF NOT EXISTS path_index ON FILES (path);
         CREATE INDEX IF NOT EXISTS parent_path_index ON FILES (parent_path);
     ";
@@ -204,6 +206,7 @@ fn get_children_of(db_path: &str, path: &str) -> String {
             tags: None,
             deadlines: None
         };
+
         files.push(file);
     }
 
