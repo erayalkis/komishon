48b3dfdf5b6f6946501f7d58daa6e398e5a67f0f
Eray Alkış <lolpo12345@outlook.com>
2022-12-19 03:37:56 +0300

    Finish fetching for tags and deadlines on get_children_of

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -188,26 +188,94 @@ fn get_base_dirs(db_path: &str) -> String {
 #[tauri::command]
 fn get_children_of(db_path: &str, path: &str) -> String {
     let conn = sqlite::open(db_path).unwrap();
-    let query = "SELECT * FROM FILES WHERE parent_path = ? ORDER BY is_dir DESC";
+    let query = 
+    "
+    SELECT F.ID AS file_id, F.file_name AS file_name, F.file_type AS file_type, F.path AS file_path, F.parent_path AS file_parent_path, F.is_dir AS is_dir, F.is_base_dir AS is_base_dir, F.byte_size AS byte_size,
+    T.ID AS tag_id, T.tag_name AS tag_name, T.parent_path as tag_parent_path, T.parent_id AS tag_parent_id, T.color AS tag_color, 
+    D.ID AS deadline_id, D.title AS title, D.date AS date, D.parent_path AS deadline_parent_path, D.parent_id AS deadline_parent_id
+    FROM FILES F
+    LEFT JOIN TAGS T ON F.ID == T.parent_id
+    LEFT JOIN DEADLINES D ON F.ID == D.parent_id
+    WHERE F.parent_path = ?
+    ORDER BY F.is_dir, F.ID;
+    ";
     let mut statement = conn.prepare(query).unwrap();
     statement.bind((1, path)).unwrap();
 
     let mut files: Vec<File> = Vec::new();
-    while let Ok(State::Row) = statement.next() {
-        let file = File {
-            id: statement.read::<i64, _>("ID").unwrap(),
-            file_name: statement.read::<String, _>("file_name").unwrap(),
-            file_type: statement.read::<String, _>("file_type").unwrap(),
-            path: statement.read::<String, _>("path").unwrap(),
-            parent_path: statement.read::<String, _>("parent_path").unwrap(),
-            is_dir: statement.read::<i64, _>("is_dir").unwrap(),
-            is_base_dir: statement.read::<i64, _>("is_base_dir").unwrap(),
-            byte_size: statement.read::<i64, _>("byte_size").unwrap(),
-            tags: None,
-            deadlines: None
-        };
+    let mut seen: HashMap<String, bool> = HashMap::new();
+    let mut last_seen_file_id: i64 = 0;
 
-        files.push(file);
+    while let Ok(State::Row) = statement.next() {
+        let file_id = statement.read::<i64, _>("file_id").unwrap();
+        let same_file_as_last = file_id == last_seen_file_id;
+
+        if !same_file_as_last {
+            let file = File {
+                id: file_id,
+                file_name: statement.read::<String, _>("file_name").unwrap(),
+                file_type: statement.read::<String, _>("file_type").unwrap(),
+                path: statement.read::<String, _>("file_path").unwrap(),
+                parent_path: statement.read::<String, _>("file_parent_path").unwrap(),
+                is_dir: statement.read::<i64, _>("is_dir").unwrap(),
+                is_base_dir: statement.read::<i64, _>("is_base_dir").unwrap(),
+                byte_size: statement.read::<i64, _>("byte_size").unwrap(),
+                tags: Some(Vec::new()),
+                deadlines: Some(Vec::new())
+            };
+
+            files.push(file);
+        }
+
+        let last_file = files.last_mut().unwrap();
+        match statement.read::<String, _>("tag_name") {
+            Ok(val) => {
+                println!("Got something: {}", val);
+                let tag_id = statement.read::<i64, _>("tag_id").unwrap();
+                let unique_tag_id = format!("{}-{}", tag_id, val);
+
+                if !seen.contains_key(&unique_tag_id) {
+                    let tag = Tag {
+                        id: Some(tag_id),
+                        tag_name: val,
+                        parent_path: statement.read::<String, _>("tag_parent_path").unwrap(),
+                        parent_id: statement.read::<i64, _>("tag_parent_id").unwrap(),
+                        color: statement.read::<String, _>("tag_color").unwrap(),
+                    };
+
+                    last_file.tags.as_mut().unwrap().push(tag);
+                    seen.insert(unique_tag_id, true);
+                }
+            }
+            Err(e) => {
+                println!("Got error: {}", e);
+            }
+        }
+
+        match statement.read::<String, _>("title") {
+            Ok(val) => {
+                let deadline_id = statement.read::<i64, _>("deadline_id").unwrap();
+                let unique_deadline_id = format!("{}-{}", deadline_id, val);
+
+                if !seen.contains_key(&unique_deadline_id) {
+                    let deadline = Deadline {
+                        id: Some(deadline_id),
+                        title: val,
+                        date: statement.read::<i64, _>("date").unwrap(),
+                        parent_path: statement.read::<String, _>("deadline_parent_path").unwrap(),
+                        parent_id: statement.read::<i64, _>("deadline_parent_id").unwrap(),
+                    };
+
+                    last_file.deadlines.as_mut().unwrap().push(deadline);
+                    seen.insert(unique_deadline_id, true);
+                }
+            }
+            Err(e) => {
+                println!("Got error: {}", e);
+            }
+        }
+
+        last_seen_file_id = file_id;
     }
 
     let serialized = serde_json::to_string(&files).unwrap();
