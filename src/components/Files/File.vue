0171f3af1195ff5bf6e629973824d9e20d1f5caa
Eray Alkış <lolpo12345@outlook.com>
2022-12-21 14:43:17 +0300

    Add SVG for files

--- src/components/Files/File.vue
+++ src/components/Files/File.vue
@@ -1,17 +1,21 @@
 <template>
   <div
-    class="w-32 h-32 border border-black cursor-pointer rounded-md select-none"
+    class="w-32 h-32 flex items-center justify-center border border-black cursor-pointer rounded-md select-none text-center"
     @dblclick="goTo(data)"
   >
-    <img v-if="data.is_dir" :src="FolderSolid" class="w-12 h-12" />
-    <h1>
-      {{ truncateFilenameIfTooLong(data.file_name) }}
-    </h1>
+    <div class="flex flex-col items-center">
+      <img v-if="data.is_dir" :src="FolderRegular" class="w-12 h-12" />
+      <img v-else :src="FileRegular" class="w-12 h-12" />
+      <h1>
+        {{ truncateFilenameIfTooLong(data.file_name) }}
+      </h1>
+    </div>
   </div>
 </template>
 <script setup>
 import { useStore } from "vuex";
-import FolderSolid from "@/assets/FolderSolid.svg?url";
+import FolderRegular from "@/assets/FolderRegular.svg?url";
+import FileRegular from "@/assets/FileRegular.svg?url";
 import { openFileWithShell } from "@/api/shell/actions.js";
 const { dispatch } = useStore();
 
