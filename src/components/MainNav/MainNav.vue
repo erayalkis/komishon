2061f5c09676799f6a33af6635a15d412652d71d
Eray Alkış <lolpo12345@outlook.com>
2022-12-21 15:35:24 +0300

    Add icon to nav

--- src/components/MainNav/MainNav.vue
+++ src/components/MainNav/MainNav.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex w-full bg-red-300 p-3">
     <input placeholder="search..." class="mx-auto" />
-    <h1 class="ml-auto mr-3">Notifications</h1>
+    <h1 class="ml-auto mr-3"><img :src="BellRegular" /></h1>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import BellRegular from "@/assets/BellRegular.svg?url";
+</script>
