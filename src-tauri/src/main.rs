6448805d3e77ab7c8b4384bfa16ddb94a21f1d82
Eray Alkış <lolpo12345@outlook.com>
2022-12-29 21:48:54 +0300

    Use helper functions in main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,9 +4,11 @@
 )]
 
 mod models;
+mod helpers;
 use models::deadline::{add_deadline_to_file, remove_deadline_from_file, update_file_deadline};
 use models::tag::{add_tag_to_file, remove_tag_from_file};
 use models::file::{get_base_dirs, get_children_of, walk_and_save, remove_invalid_files_from_db};
+use helpers::database::create_db_if_not_exists;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
