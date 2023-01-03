27ef46d553f6bbc5d2e9f8ded229a2425e5b012c
Eray Alkış <lolpo12345@outlook.com>
2023-01-04 00:01:16 +0300

    Add file explorer opt to context menu

--- src/components/ContextMenu/Items/FolderItems.vue
+++ src/components/ContextMenu/Items/FolderItems.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Item Folder</div>
+  <div @click="view">View in file explorer</div>
 </template>
+<script setup>
+import { useStore } from "vuex";
+import { openFileWithShell } from "@/api/shell/actions.js";
+
+const { getters } = useStore();
+
+const props = defineProps({
+  targetId: {
+    type: String,
+    default: 0,
+  },
+});
+
+const view = () => {
+  const file = getters.getFile(props.targetId);
+  openFileWithShell(file.path);
+};
+</script>
