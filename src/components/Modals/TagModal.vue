64eddebce7ecf0e6901eb0bae201ed7a89ffb7c2
Eray Alkış <lolpo12345@outlook.com>
2023-01-05 18:29:24 +0300

    Finish tag modal

--- src/components/Modals/TagModal.vue
+++ src/components/Modals/TagModal.vue
@@ -2,7 +2,7 @@
   <BaseModal>
     <template #header>
       <div class="flex p-3 items-center justify-between">
-        <h1 class="text-3xl text-gray-900">Add a tag</h1>
+        <h1 class="text-4xl text-gray-900">Add a tag</h1>
         <img
           :src="X"
           class="mr-3 cursor-pointer"
@@ -11,14 +11,31 @@
       </div>
     </template>
     <template #body>
-      <div class="flex">
-        <h1>Pick a colour</h1>
-        <input type="color" />
+      <div class="flex-col">
+        <div class="flex p-3 items-center justify-between w-96">
+          <h1 class="text-3xl">Pick a colour:</h1>
+          <input type="color" class="w-24 h-24" />
+        </div>
+        <div class="flex p-3 items-center justify-between w-96">
+          <h1 class="text-3xl">Pick a name:</h1>
+          <input class="indent-3 text-gray-900 p-1 bg-gray-200 rounded-md" />
+        </div>
       </div>
     </template>
     <template #footer>
-      <button @click="$emit('closeTagModal')">Cancel</button>
-      <button>Save</button>
+      <div class="flex gap-3 justify-center">
+        <button
+          @click="$emit('closeTagModal')"
+          class="w-32 h-24 bg-violet-600 text-white hover:bg-violet-700 transition duration-300 ease-out"
+        >
+          Cancel
+        </button>
+        <button
+          class="w-32 h-24 bg-violet-600 text-white hover:bg-violet-700 transition duration-300 ease-out"
+        >
+          Save
+        </button>
+      </div>
     </template>
   </BaseModal>
 </template>
