57ef71c1a770a9750854e937a619a4df094f1c6f
Eray Alkış <lolpo12345@outlook.com>
2023-01-14 01:07:02 +0300

    Create base version of deadlines page

--- src/components/Deadlines/Deadlines.vue
+++ src/components/Deadlines/Deadlines.vue
@@ -1,14 +1,22 @@
 <template>
   <template v-for="(val, key) in deadlines">
-    {{ key }}
-    <template v-for="file in val">
-      {{ file.file_name }}
-    </template>
+    <h1 class="text-3xl text-gray-900 px-1">
+      {{ new Date(key * 1000).toDateString() }}
+    </h1>
+    <br />
+    <div class="flex gap-2 p-3 py-2">
+      <template v-for="file in val">
+        <File :data="file" />
+      </template>
+    </div>
+    <br />
   </template>
 </template>
 <script setup>
 import { onMounted, computed } from "vue";
 import { useStore } from "vuex";
+import File from "@/components/Files/File.vue";
+
 const { dispatch, state } = useStore();
 
 const deadlines = computed(() => state.deadlines.deadlines);
