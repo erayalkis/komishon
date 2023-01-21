0e0613aa8dff44d3503dc9ed7ce5b38628663904
Eray Alkış <lolpo12345@outlook.com>
2023-01-21 03:53:29 +0300

    Style changes

--- src/components/Files/File.vue
+++ src/components/Files/File.vue
@@ -2,7 +2,7 @@
   <div
     :class="{ folder: data.is_dir, file: !data.is_dir }"
     :component-id="data.id"
-    class="w-40 h-40 bg-stone-50 border border-gray-300 flex items-center relative justify-center cursor-pointer rounded-md select-none text-center hover:shadow-md transition duration-300 ease-out"
+    class="w-40 h-40 bg-stone-50 border border-gray-300 flex items-center relative justify-center cursor-pointer rounded-md select-none text-center hover:shadow-lg transition duration-300 ease-out"
     @dblclick="goTo(data)"
   >
     <svg
--- src/components/Nav/Navbar.vue
+++ src/components/Nav/Navbar.vue
@@ -5,26 +5,25 @@
     <NavHeader class="text-center p-3" />
     <router-link to="/">
       <div
-        class="flex items-center text-xl text-gray-900 w-full px-2 py-1.5 hover:bg-violet-500 hover:text-white transition duration-200 ease-out cursor-pointer"
+        class="nav-home flex items-center text-xl text-gray-900 w-full px-2 py-1.5 hover:bg-violet-500 hover:text-zinc-100 transition duration-200 ease-out cursor-pointer"
       >
-        <img :src="Home" class="h-6 w-6 mr-1" />
-
+        <HomeSvg class="h-6 w-6 mr-1" />
         <h1>Home</h1>
       </div>
     </router-link>
     <router-link to="/favorites">
       <div
-        class="text-xl flex text-gray-900 w-full px-2 py-1.5 hover:bg-violet-500 hover:text-white transition duration-200 ease-out cursor-pointer"
+        class="text-xl flex text-gray-900 w-full px-2 py-1.5 hover:bg-violet-500 hover:text-zinc-100 transition duration-200 ease-out cursor-pointer"
       >
-        <img :src="Heart" class="h-6 w-6 mr-1" />
+        <HeartSvg class="h-6 w-6 mr-1" />
         <h1>Favorites</h1>
       </div>
     </router-link>
     <router-link to="/deadlines">
       <div
-        class="flex items-center text-xl text-gray-900 w-full px-2 py-1.5 hover:bg-violet-500 hover:text-white transition duration-200 ease-out cursor-pointer"
+        class="flex items-center text-xl text-gray-900 w-full px-2 py-1.5 hover:bg-violet-500 hover:text-zinc-100 transition duration-200 ease-out cursor-pointer"
       >
-        <img :src="Calendar" class="h-6 w-6 mr-1" />
+        <CalendarSvg class="h-6 w-6 mr-1" />
         <h1>Deadline Report</h1>
       </div>
     </router-link>
@@ -37,12 +36,15 @@
 <script setup>
 import NavHeader from "./NavHeader.vue";
 import Calendar from "@/assets/Calendar.svg?url";
-import Home from "@/assets/Home.svg?url";
 import Heart from "@/assets/Heart.svg?url";
 import { getVersion } from "@tauri-apps/api/app";
 import { onMounted, ref } from "vue";
+import HomeSvg from "../../assets/svgComponents/HomeSvg.vue";
+import HeartSvg from "../../assets/svgComponents/HeartSvg.vue";
+import CalendarSvg from "../../assets/svgComponents/CalendarSvg.vue";
 
 const tauriVersion = ref(null);
+
 onMounted(async () => {
   tauriVersion.value = await getVersion();
 });
