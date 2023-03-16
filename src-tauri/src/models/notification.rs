a0cd112cf0257f3428fc220922ad8742eca0db13
Eray Alkış <lolpo12345@outlook.com>
2023-03-16 04:16:39 +0300

    Fix errors in norification.ers

--- src-tauri/src/models/notification.rs
+++ src-tauri/src/models/notification.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use sqlite::Error;
 
 use crate::helpers::database::get_db;
 
@@ -9,18 +10,18 @@ pub struct Notification {
   pub body: String,
 }
 
-pub fn create_notification(title: String, body: String) -> Result<Notification, &'static &str> {
+pub fn create_notification(title: String, body: String) -> Result<Notification, Error> {
   let conn = get_db().unwrap();
   let statement = "INSERT INTO NOTIFICATIONS(title, body) VALUES (?, ?) RETURNING *";
 
   let mut query = conn.prepare(statement).expect("Error while preparing SQL statement");
-  query.bind((1, title)).unwrap();
-  query.bind((2, body)).unwrap();
+  query.bind((1, &title[..])).unwrap();
+  query.bind((2, &body[..])).unwrap();
 
   match query.next() {
     Ok(_) => {
       let notification = Notification {
-        id: query.read::<i64, _>("id"),
+        id: Some(query.read::<i64, _>("id").unwrap()),
         title,
         body
       };
@@ -35,7 +36,7 @@ pub fn create_notification(title: String, body: String) -> Result<Notification, 
   }
 }
 
-pub fn delete_notification(id: i64) -> Result<Notification, &'static &str> {
+pub fn delete_notification(id: i64) -> Result<Notification, Error> {
   let conn = get_db().unwrap();
   let statement = "DELETE FROM NOTIFICATIONS WHERE ID = ? RETURNING *";
   
@@ -46,8 +47,8 @@ pub fn delete_notification(id: i64) -> Result<Notification, &'static &str> {
     Ok(_) => {
       let notification = Notification {
         id: Some(id),
-        title: query.read::<String, _>("title"),
-        body: query.read::<String, _>("body")
+        title: query.read::<String, _>("title").unwrap(),
+        body: query.read::<String, _>("body").unwrap()
       };
 
       Ok(notification)
