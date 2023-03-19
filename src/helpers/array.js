eac275d4e45ce864d0bdc31205e9bdacd6a62252
Eray Alkış <lolpo12345@outlook.com>
2023-03-19 16:19:58 +0300

    Write comment doc for array helpers

--- src/helpers/array.js
+++ src/helpers/array.js
@@ -1,3 +1,5 @@
+// Returns the last element of an array
+// TODO: Make all lines using this function switch to the `at` function.
 export function lastEleOf(arr) {
   const lastEleIdx = arr.length - 1;
   return arr[lastEleIdx];
