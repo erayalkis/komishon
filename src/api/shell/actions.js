4f18865f0e1a4279299cff8753be6b5aa776c203
Eray Alkış <lolpo12345@outlook.com>
2023-03-19 16:15:26 +0300

    Finish comment docs for api files

--- src/api/shell/actions.js
+++ src/api/shell/actions.js
@@ -1,5 +1,7 @@
 import { open } from "@tauri-apps/api/shell";
 
+// Uses the Tauri API's `open` function to open a file or a folder using the native shell.
+// It works with any file extensions which have an associated app in the $PATH.
 export async function openFileWithShell(filepath) {
   open(filepath);
 }
